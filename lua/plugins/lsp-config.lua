d27a54ce0486106a8151f1f7385606c633c32003
Brad <cloudmetal4430@gmail.com>
2025-10-19 23:29:32 +0100

    updated lsp-config, added autocompletion, gitsigns

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,25 +1,148 @@
 return {
-	'neovim/nvim-lspconfig',
+	"neovim/nvim-lspconfig",
 	dependencies = {
-		{ 'mason-org/mason.nvim', config = true },
-		'mason-org/mason-lspconfig.nvim',
-		'WhoIsSethDaniel/mason-tool-installer.nvim',
+		{ "mason-org/mason.nvim", opts = {} },
+		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{
+			"j-hui/fidget.nvim",
+			opts = {
+				notification = {
+					window = {
+						winblend = 0, -- Background color opacity in the notification window
+					},
+				},
+			},
+		},
+
+		-- Allows extra capabilities provided by nvim-cmp
+		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+			callback = function(event)
+				-- Create a function that lets us more easily define mappings specific
+				-- for LSP related items. It sets the mode, buffer and description for us each time.
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
+
+				-- Jump to the definition of the word under your cursor.
+				--  This is where a variable was first declared, or where a function is defined, etc.
+				--  To jump back, press <C-t>.
+				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+				-- Find references for the word under your cursor.
+				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+				-- Jump to the implementation of the word under your cursor.
+				--  Useful when your language has ways of declaring types without an actual implementation.
+				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+				-- Jump to the type of the word under your cursor.
+				--  Useful when you're not sure what type a variable is and you want to see
+				--  the definition of its *type*, not where it was *defined*.
+				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+
+				-- Fuzzy find all the symbols in your current document.
+				--  Symbols are things like variables, functions, types, etc.
+				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+				-- Fuzzy find all the symbols in your current workspace.
+				--  Similar to document symbols, except searches over your entire project.
+				-- map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+				-- Rename the variable under your cursor.
+				--  Most Language Servers support renaming across files, etc.
+				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+				-- Execute a code action, usually your cursor needs to be on top of an error
+				-- or a suggestion from your LSP for this to activate.
+				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+				-- WARN: This is not Goto Definition, this is Goto Declaration.
+				--  For example, in C this would take you to the header.
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+				vim.api.nvim_create_autocmd("LspDetach", {
+					group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+					callback = function(event2)
+						vim.lsp.buf.clear_references()
+						vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+					end,
+				})
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+				-- The following code creates a keymap to toggle inlay hints in your
+				-- code, if the language server you are using supports them
+				if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+					map("<leader>th", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					end, "[T]oggle Inlay [H]ints")
+				end
+			end,
+		})
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
 		local servers = {
+			html = {},
+			cssls = {},
+			ts_ls = {
+				--	enabled = false,
+				--cmd = { "typescript-language-server", "--stdio" },
+				--workspace_required = true,
+
+				--root_dir = function(fname)
+				--	local util = require("lspconfig.util")
+				--	local root = util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git")(fname)
+
+				-- Fix for WSL returning a table instead of string
+				--	if type(root) == "table" then
+				--		root = root[1]
+				--	end
+
+				--Fallback for one-off files
+				--	return root or vim.fs.dirname(fname)
+				--end,
+				--single_file_support = true,
+				flags = { debounce_text_changes = 300 },
+			},
+			pyright = {
+				settings = {
+					python = {
+						analysis = {
+							ignore = { "*" },
+						},
+					},
+				},
+			},
+			ruff = {
+				on_attach = function(client)
+					client.server_capabilities.hoverProvider = false
+				end,
+				init_options = {
+					settings = {
+						args = {},
+					},
+				},
+			},
 			lua_ls = {
 				settings = {
 					Lua = {
 						completion = {
-							callSnippet = 'Replace',
+							callSnippet = "Replace",
 						},
-						runtime = { version = 'LuaJIT' },
+						runtime = { version = "LuaJIT" },
 						workspace = {
 							checkThirdParty = false,
-							library = vim.api.nvim_get_runtime_file('', true),
+							library = vim.api.nvim_get_runtime_file("", true),
 						},
 						diagnostics = {
-							globals = { 'vim' },
+							globals = { "vim" },
 						},
 						format = {
 							enable = false,
@@ -27,16 +150,49 @@ return {
 					},
 				},
 			},
+			--vtsls = {
+			--	root_dir = require("lspconfig.util").root_pattern(
+			--		"package.json",
+			--		"jsconfig.json",
+			--		"tsconfig.json",
+			--		".git"
+			--	) or vim.fn.getcwd(),
+			--	single_file_support = true,
+			--	flags = { debounce_text_changes = 300 },
+			--},
 		}
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
-			'stylua',
+			"stylua",
 		})
-		require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+		local old_servers = {
+			--"ts_ls",
+			"vtsls",
+		}
+
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+		local function inTable(tbl, item)
+			for key, value in pairs(tbl) do
+				if value == item then
+					return true
+				end
+			end
+			return false
+		end
 
 		for server, cfg in pairs(servers) do
+			cfg.capabilities = vim.tbl_deep_extend("force", {}, capabilities, cfg.capabilities or {})
+			--if inTable(old_servers, server) then
+			--	local server_cfg = servers[server] or {}
+			--	server_cfg.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server_cfg.capabilities or {})
+
+			--	require("lspconfig")[server].setup(server_cfg)
+			--else
 			vim.lsp.config(server, cfg)
 			vim.lsp.enable(server)
+			--end
 		end
 	end,
 }
